46453c5f3eb41f9cc34daac79cf5f2f243c0d8be
William <william.kamarainen@gmail.com>
2020-09-08 10:25:15 +0200

    done with getting all styles from props and add random text in the end

--- src/components/form/buttons/buttons.tsx
+++ src/components/form/buttons/buttons.tsx
@@ -13,21 +13,30 @@ export const Buttons = (props: any) => {
   console.log("props in obj: ", props);
   console.log("props in array", Object.keys(props));
   console.log("styles:", styles);
+  console.log("classnames: ", classnames);
 
-  const classInArr = classnames.split(" ").map((x) => {
-    if (x == "primary") {
-      console.log(x, styles.primary);
-      x.concat(styles.primary);
-    } else {
-      console.log(x, styles.secondary);
-      x.concat(styles.secondary);
-    }
+  const classNamesWithStyles = classnames.split(" ").map((c) => {
+    return Object.keys(styles).map((s) => {
+      if (s === c) {
+        return (c += styles[s]);
+      }
+    });
   });
 
-  console.log(classInArr);
-  console.log(classnames);
+  var removeUndefinedFromArray: string[] = [];
 
-  return <button className={classnames}>{text}</button>;
+  classNamesWithStyles.map((x) => {
+    return x.map((z) => {
+      if (z !== undefined) {
+        removeUndefinedFromArray.push(z);
+      }
+    });
+  });
+
+  let completeClassNames = removeUndefinedFromArray.join(" ");
+  console.log(completeClassNames);
+
+  return <button className={completeClassNames}>{text}</button>;
 };
 
 export default Buttons;
